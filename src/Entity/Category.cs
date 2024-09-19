9329c7904810a5c527ca33b4a5f467911c76d59c
etharalrehaili <e.abdullah.re@gmail.com>
2024-09-19 17:02:56 +0300

    Add Category.cs

---
+++ src/Entity/Category.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Entity
+{
+    public class Category
+    {
+        public int Category_Id { get; set; }
+        public string Category_Name { get; set; }
+    }
+}
