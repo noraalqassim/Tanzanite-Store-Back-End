6564a8f737695fadf7c77f25c3abf72c97c5fb58
noraalqassim <noraalqassim12@gmail.com>
2024-09-19 17:18:27 +0300

    Add Jewlery and Gemstone

---
+++ src/Entity/Gemstone.cs
@@ -0,0 +1,12 @@
+namespace src.Entity
+{
+    public class Gemstones
+    {
+        public int Gemstone_ID { get; set; }
+        public string Gemstone_Type { get; set; }
+        public string Gemstone_Color { get; set; }
+        public string Gemstone_Image { get; set; }
+        public string Gemstone_Clarity { get; set; }
+        public string Gemstone_Description { get; set; }
+    }
+}
