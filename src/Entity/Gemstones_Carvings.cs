2cb7ab00a90d8a71a5c073b6846fa35df6fd71ad
noraalqassim <noraalqassim12@gmail.com>
2024-09-23 00:39:36 +0300

    Add Gemstone_Carving

---
+++ src/Entity/Gemstones_Carvings.cs
@@ -0,0 +1,13 @@
+namespace src.Entity
+{
+    public class Gemstones_Carvings
+    {
+        public int Carving_ID { get; set; }
+    public string Carving_Name { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Price { get; set; }
+    public string Carving_Info { get; set; }
+    public string Image { get; set; }
+    public int Seller_ID { get; set; }
+    }
+}
