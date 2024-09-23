aebeaccd06b74d0323f80005cea8ec08bf6f86c2
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-23 22:00:24 +0300

    #1 - Add list payments to PaymentsController

--- src/Controllers/PaymentsController.cs
+++ src/Controllers/PaymentsController.cs
@@ -8,6 +8,12 @@ namespace src.Controllers
 
     public class PaymentsController : ControllerBase
     {
-
+        // Payment Entity
+        List<Payment> payments = new List<Payment>
+        {
+            new Payment {PaymentId = 1, PaymentDate = new DateTime(2023, 1, 1), Amount = 100.00f, PaymentOption = "Card"},
+            new Payment {PaymentId = 2, PaymentDate = new DateTime(2023, 2, 1), Amount = 250.25f, PaymentOption = "Cash"},
+            new Payment {PaymentId = 3, PaymentDate = new DateTime(2023, 3, 1), Amount = 375.50f, PaymentOption = "Card"},
+        };
     }
 }
