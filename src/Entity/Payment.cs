268bd97ac66db643bd7262d2588ef5d8ec7d8a38
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-23 21:54:35 +0300

    #1 - Create Payment.cs

---
+++ src/Entity/Payment.cs
@@ -0,0 +1,10 @@
+namespace GemstoneStore
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public float Amount { get; set; }
+        public string PaymentOption { get; set; }
+    }
+}
