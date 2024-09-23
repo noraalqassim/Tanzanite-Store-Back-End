77d68d3da19a8ef1a7636bd680283816e1c8e416
R-1493 <retajharbi319@gmail.com>
2024-09-23 17:09:38 +0300

    Controolers And Entity For All User and seller (get, put,post, delete)

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -11,11 +11,16 @@ namespace src.Controllers
     [ApiController] //User
     public class UserController : ControllerBase
     {
-        public List<Users> users = new List<Users>
+        /// <summary>
+        /// make the list static so all the sellers and the customers access it.
+        /// make lis info.
+        /// get, post, put and delete method for user without consider is(seller, customer).
+        /// </summary>
+        public static List<Users> users = new List<Users>
         {
             new Users
             {
-                UserId = 2,
+                UserId = 1,
                 Name = "Retaj",
                 PhoneNumber = "+966222222222",
                 Email = "Retaj@example.com",
@@ -25,7 +30,7 @@ namespace src.Controllers
             },
             new Users
             {
-                UserId = 1,
+                UserId = 2,
                 Name = "Nora",
                 PhoneNumber = "+966111111111",
                 Email = "Nora@example.com",
@@ -35,7 +40,7 @@ namespace src.Controllers
             },
             new Users
             {
-                UserId = 4,
+                UserId = 3,
                 Name = "Ethra",
                 PhoneNumber = "+966333333333",
                 Email = "Ethra@example.com",
@@ -45,7 +50,7 @@ namespace src.Controllers
             },
             new Users
             {
-                UserId = 5,
+                UserId = 4,
                 Name = "Weed",
                 PhoneNumber = "+9664444444444",
                 Email = "Weed@example.com",
@@ -55,7 +60,7 @@ namespace src.Controllers
             },
             new Users
             {
-                UserId = 6,
+                UserId = 5,
                 Name = "Rahaf",
                 PhoneNumber = "+966555555555",
                 Email = "Rahaf@example.com",
@@ -70,10 +75,72 @@ namespace src.Controllers
         {
             if (users.Count == 0)
             {
-                return Ok("there is no user yet!");
+                return NotFound("it's empty!");
             }
 
             return Ok(users);
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult GetUserByName(string name)
+        {
+            var user = users.FirstOrDefault(i => i.Name == name);
+            if (users == null || users.Count == 0)
+            {
+                return NotFound("there is no user in this name");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult PostUser(Users newUser)
+        {
+            var existingUser = users.FirstOrDefault(u => u.UserId == newUser.UserId);
+
+            if (existingUser != null)
+            {
+                return Conflict($"User with ID {newUser.UserId} already exists");
+            }
+
+            users.Add(newUser);
+
+            return Ok($"New user created: {newUser}");
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateUser(int id, Users updatedUser)
+        {
+            var existingUser = users.FirstOrDefault(i => i.UserId == id);
+
+            if (existingUser == null)
+            {
+                return NotFound("User not found");
+            }
+
+            existingUser.Name = updatedUser.Name;
+            existingUser.PhoneNumber = updatedUser.PhoneNumber;
+            existingUser.Email = updatedUser.Email;
+            existingUser.Password = updatedUser.Password;
+            existingUser.IsCustomer = updatedUser.IsCustomer;
+            existingUser.IsSeller = updatedUser.IsSeller;
+
+            return Ok($"User information updated: {existingUser}");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id)
+        {
+            var userToDelete = users.FirstOrDefault(u => u.UserId == id);
+
+            if (userToDelete == null)
+            {
+                return NotFound("User not found");
+            }
+
+            users.Remove(userToDelete);
+
+            return Ok($"User with ID {id} deleted successfully");
         }
     }
 }
