7e7f9d5b9f856b04c8ae56536077aa766a054ba9
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-28 20:24:59 +0300

    # 27 - Add dbset Payment and PaymentCard

--- src/Database/DatabaseContext.cs
+++ src/Database/DatabaseContext.cs
@@ -17,6 +17,8 @@ namespace src.Database
         public DbSet<Gemstones_Carvings> Gemstones_Carvings { get; set; }
         public DbSet<Gemstones> Gemstones { get; set; }
         public DbSet<Jewelry> Jewelry { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        public DbSet<PaymentCard> PaymentCard { get; set; }
 
         public DatabaseContext(DbContextOptions options)
             : base(options) { }
