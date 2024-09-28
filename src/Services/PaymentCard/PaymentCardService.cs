67b0c5a81b9c5dd31568eab7823e9ecbff76cad1
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-28 23:30:06 +0300

    #35 - Implement CreateOneAsync()

--- src/Services/PaymentCard/PaymentCardService.cs
+++ src/Services/PaymentCard/PaymentCardService.cs
@@ -15,5 +15,12 @@ namespace src.Services.PaymentCard
             _mapper = mapper;
         }
 
+        public async Task<PaymentCardReadDto> CreateOneAsync(PaymentCardCreateDto createDto)
+        {
+            var paymentCard = _mapper.Map<PaymentCardCreateDto, Entity.Payment>(createDto);
+            var paymentCardCreated = await _paymentCardRepo.CreateOneAsync(paymentCard);
+            return _mapper.Map<Entity.Payment, PaymentCardReadDto>(paymentCardCreated);
+        }
+
     }
 }
