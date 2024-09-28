d12538f415123bfa09bfb531fdbc3577dc07eb21
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-28 23:14:14 +0300

    #32 - Implement UpdateOneAsync()

--- src/Services/Payment/PaymentService.cs
+++ src/Services/Payment/PaymentService.cs
@@ -22,25 +22,40 @@ namespace src.Services.Payment
             return _mapper.Map<Entity.Payment, PaymentReadDto>(paymentCreated);
         }
 
-        public async Task<List<PaymentReadDto>> GetAllAsync(){
+        public async Task<List<PaymentReadDto>> GetAllAsync()
+        {
             var paymentList = await _paymentRepo.GetAllAsync();
             return _mapper.Map<List<Entity.Payment>, List<PaymentReadDto>>(paymentList);
         }
 
-        public async Task<PaymentReadDto> GetByIdAsync(Guid id){
+        public async Task<PaymentReadDto> GetByIdAsync(Guid id)
+        {
             var foundPayment = await _paymentRepo.GetByIdAsync(id);
             // TODO: Add error handling
             return _mapper.Map<Entity.Payment, PaymentReadDto>(foundPayment);
         }
 
-        public async Task<bool> DeleteOneAsync(Guid id){
+        public async Task<bool> DeleteOneAsync(Guid id)
+        {
             var foundPayment = await _paymentRepo.GetByIdAsync(id);
             bool isDeleted = await _paymentRepo.DeleteOneAsync(foundPayment);
             if (isDeleted)
             {
                 return true;
-            } 
+            }
             return false;
+        }
+
+        public async Task<bool> UpdateOneAsync(Guid id, PaymentUpdateDto updateDto)
+        {
+            var foundPayment = await _paymentRepo.GetByIdAsync(id);
+            bool isUpdated = await _paymentRepo.UpdateOneAsync(foundPayment);
+            if (isUpdated)
+            {
+                return false;
+            }
+            _mapper.Map(updateDto, foundPayment);
+            return await _paymentRepo.UpdateOneAsync(foundPayment);
         }
     }
 }
